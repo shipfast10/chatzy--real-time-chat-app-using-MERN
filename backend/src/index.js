fcb8365c147dfdfe55e218059e5b30d8a3a2794a
Bhavya Agarwal <bhavyaagarwal675@gmail.com>
2025-07-19 15:31:06 +0530

    deployed

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://chatzy-neon.vercel.app/"],
     credentials: true,
   })
 );
