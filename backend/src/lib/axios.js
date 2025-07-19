ea2ca6afa0874c8223c7736320e55e218f1b800a
Bhavya Agarwal <bhavyaagarwal675@gmail.com>
2025-07-19 16:02:10 +0530

    deploying

--- backend/src/lib/axios.js
+++ backend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5001";
+const BASE_URL = `${import.meta.env.VITE_BACKEND_URL || "http://localhost:5001"}/api`;
 
 export const axiosInstance = axios.create({
     baseURL: BASE_URL,
