ef5faab87a87815703b003e566c38b8bec39e36f
Bhavya Agarwal <bhavyaagarwal675@gmail.com>
2025-08-21 15:31:06 +0530

    Fixed realtime issue

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,19 @@
 import { useChatStore } from "../store/useChatStore";
+import { useEffect } from "react";
 
 import Sidebar from "../components/Sidebar";
 import NoChatSelected from "../components/NoChatSelected";
 import ChatContainer from "../components/ChatContainer";
 
 const HomePage = () => {
-  const { selectedUser } = useChatStore();
+  const { selectedUser, subscribeToMessages, unsubscribeFromMessages } = useChatStore();
+
+  useEffect(() => {
+    subscribeToMessages();
+    return () => {
+      unsubscribeFromMessages();
+    };
+  }, [subscribeToMessages, unsubscribeFromMessages]);
 
   return (
     <div className="h-screen bg-base-200">
